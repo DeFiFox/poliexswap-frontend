22493eb6bf1b4a9893e94d595c3060c7655391b6
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-05-29 22:09:23 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -22,77 +22,7 @@ const farms: FarmConfig[] = [
     token: tokens.busd,
     quoteToken: tokens.wbnb,
   },
-  {
-    pid: 4,
-    lpSymbol: 'BNB-USDT LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE',
-    },
-    token: tokens.usdt,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 6,
-    lpSymbol: 'ETH-USDT LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x74E4716E431f45807DCF19f284c7aA99F18a4fbc',
-    },
-    token: tokens.usdt,
-    quoteToken: tokens.eth,
-  },
-  {
-    pid: 8,
-    lpSymbol: 'BTC-BNB LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x61EB789d75A95CAa3fF50ed7E47b96c132fEc082',
-    },
-    token: tokens.btcb,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 5,
-    lpSymbol: 'USDT-BUSD LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x7EFaEf62fDdCCa950418312c6C91Aef321375A00',
-    },
-    token: tokens.wbnb,
-    quoteToken: tokens.busd,
-  },
-  {
-    pid: 10,
-    lpSymbol: 'BTC-BUSD LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0xf45cd219aef8618a92baa7ad848364a158a24f33',
-    },
-    token: tokens.busd,
-    quoteToken: tokens.btcb,
-  },
-  {
-    pid: 3,
-    lpSymbol: 'USDC-BUSD LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x2354ef4DF11afacb85a5C7f98B624072ECcddbB1',
-    },
-    token: tokens.usdc,
-    quoteToken: tokens.busd,
-  },
-  
-  {
-    pid: 9,
-    lpSymbol: 'CAKE-BNB LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
-    },
-    token: tokens.cake,
-    quoteToken: tokens.wbnb,
-  },
+ 
 ]
 
 export default farms
