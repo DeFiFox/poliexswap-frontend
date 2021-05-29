b2623e71f910972ef831330d0828abbb7f20dde0
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-05-29 22:02:20 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -12,7 +12,16 @@ const farms: FarmConfig[] = [
     token: tokens.beco,
     quoteToken: tokens.wbnb,
   },
-  
+  {
+    pid: 2,
+    lpSymbol: 'BNB-BUSD LP',
+    lpAddresses: {
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
   {
     pid: 4,
     lpSymbol: 'BNB-USDT LP',
