14e4917632c2cb716e6e52a6e3dd960aa10011a3
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-01 14:39:52 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -4,15 +4,15 @@ import { PoolConfig, PoolCategory } from './types'
 const pools: PoolConfig[] = [
   {
     sousId: 0,
-    stakingToken: tokens.beco,
-    earningToken: tokens.beco,
+    stakingToken: tokens.polex,
+    earningToken: tokens.polex,
     contractAddress: {
        97: '',
        56: '0xD1CeA1Ed16e618AcBc1e0F87bBe6A19d4Ae03baa',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '10',
+    tokenPerBlock: '100',
     sortOrder: 1,
     isFinished: false,
   }
