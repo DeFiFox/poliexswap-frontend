831396507621def98ebc7a0781431993d6518059
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-01 17:01:41 +0700

    Update BountyCard.tsx

--- src/views/Pools/components/BountyCard.tsx
+++ src/views/Pools/components/BountyCard.tsx
@@ -55,11 +55,11 @@ const BountyCard = () => {
       <Text mb="16px">{`${t(`This bounty is given as a reward for providing a service to other users.`)}`}</Text>
       <Text mb="16px">
         {t(
-          'Whenever you successfully claim the bounty, you’re also helping out by activating the Auto BECO Pool’s compounding function for everyone.',
+          'Whenever you successfully claim the bounty, you’re also helping out by activating the Auto POLEX Pool’s compounding function for everyone.',
         )}
       </Text>
       <Text style={{ fontWeight: 'bold' }}>
-        {t(`Auto-Compound Bounty: %fee%% of all Auto BECO pool users’ pending yield`, { fee: callFee / 100 })}
+        {t(`Auto-Compound Bounty: %fee%% of all Auto POLEX pool users’ pending yield`, { fee: callFee / 100 })}
       </Text>
     </>
   )
@@ -87,7 +87,7 @@ const BountyCard = () => {
           <Flex flexDirection="column">
             <Flex alignItems="center" mb="12px">
               <Text fontSize="16px" bold color="textSubtle" mr="4px">
-                {t('Auto BECO Bounty')}
+                {t('Auto POLEX Bounty')}
               </Text>
               <Box ref={targetRef}>
                 <HelpIcon color="textSubtle" />
