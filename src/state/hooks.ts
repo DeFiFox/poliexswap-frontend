fc57c5b3e9958e63ee8ea31d5c93bf7548838c98
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-01 18:15:10 +0700

    Update hooks.ts

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -202,7 +202,7 @@ export const useGetApiPrice = (address: string) => {
 }
 
 export const usePriceBnbBusd = (): BigNumber => {
-  const bnbBusdFarm = useFarmFromPid(2)
+  const bnbBusdFarm = useFarmFromPid(4)
   return bnbBusdFarm.tokenPriceVsQuote ? new BigNumber(1).div(bnbBusdFarm.tokenPriceVsQuote) : BIG_ZERO
 }
 
