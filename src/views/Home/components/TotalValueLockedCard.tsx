bb43878fb72734e91d4478bb17d790918c32bf31
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-10 22:55:26 +0700

    Update TotalValueLockedCard.tsx

--- src/views/Home/components/TotalValueLockedCard.tsx
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -1,37 +1,75 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Card, CardBody, Heading, Skeleton, Text } from '@becoswap-libs/uikit'
+import { Heading, Text, BaseLayout } from '@becoswap-libs/uikit'
 import { useTranslation } from 'contexts/Localization'
-import { useGetStats } from 'hooks/api'
+import Page from 'components/layout/Page'
+import FarmStakingCard from 'views/Home/components/FarmStakingCard'
+import LotteryCard from 'views/Home/components/LotteryCard'
+import CakeStats from 'views/Home/components/CakeStats'
+import TotalValueLockedCard from 'views/Home/components/TotalValueLockedCard'
 
-const StyledTotalValueLockedCard = styled(Card)`
+const Hero = styled.div`
+  background-image: url('/images/pan-bg.png');
+  -webkit-box-align: center;
   align-items: center;
+  background-position: center center, center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  box-shadow: rgb(146 146 146 / 35%) 0px 0px 0px 3000px inset;
   display: flex;
-  flex: 1;
+  -webkit-box-pack: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto auto 25px;
+  padding: 32px 16px;
+  text-align: center;
 `
 
-const TotalValueLockedCard = () => {
+const Cards = styled(BaseLayout)`
+  align-items: stretch;
+  justify-content: stretch;
+  margin-bottom: 32px;
+  & > div {
+    grid-column: span 6;
+    width: 100%;
+  }
+  ${({ theme }) => theme.mediaQueries.sm} {
+    & > div {
+      grid-column: span 8;
+    }
+  }
+  ${({ theme }) => theme.mediaQueries.lg} {
+    & > div {
+      grid-column: span 6;
+    }
+  }
+`
+
+const Home: React.FC = () => {
   const { t } = useTranslation()
-  const data = useGetStats()
-  const tvl = data ? data.tvl.toLocaleString('en-US', { maximumFractionDigits: 0 }) : null
 
   return (
-    <StyledTotalValueLockedCard>
-      <CardBody>
-        <Heading size="lg" mb="24px">
-          {t('Total Value Locked (TVL)')}
+    <div>
+      <Hero>
+        <Heading as="h1" size="xl" mb="24px" color="secondary">
+          {t('PoliExSwap')}
         </Heading>
-        {data ? (
-          <>
-            <Heading size="xl">{`$${tvl}`}</Heading>
-            <Text color="textSubtle">{t('Across all LPs and Beco Pools')}</Text>
-          </>
-        ) : (
-          <Skeleton height={66} />
-        )}
-      </CardBody>
-    </StyledTotalValueLockedCard>
+        <Text>{t('First High Yield Farm & AMM on Binance Smart Chain.')}</Text>
+      </Hero>
+      <Page>
+        <div>
+          <Cards>
+            <FarmStakingCard />
+            <LotteryCard />
+          </Cards>
+          <Cards>
+            <CakeStats />
+            <TotalValueLockedCard />
+          </Cards>
+        </div>
+      </Page>
+    </div>
   )
 }
 
-export default TotalValueLockedCard
+export default Home
