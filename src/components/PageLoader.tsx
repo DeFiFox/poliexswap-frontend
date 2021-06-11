3d2e7ac87b5c92ceb2070211a1881f52006955c7
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-11 13:26:02 +0700

    Update PageLoader.tsx

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Text } from '@becoswap-libs/uikit'
+import { useTranslation } from 'contexts/Localization'
 import Page from './layout/Page'
 
 const Wrapper = styled(Page)`
